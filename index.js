05c202f635b062780124959b90e24867e8363373
j21O2 <166688413+j21O2@users.noreply.github.com>
2024-05-16 07:02:53 +0000

    poranko

--- index.js
+++ index.js
@@ -2,10 +2,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!test,5月16日雨')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// GETリクエスト
+app.get('/', (req, res) => {
+  res.send({
+    msg:'GET request'
+  });
+});
+
+// POSTリクエスト
+app.post('/', (req, res) => {
+  res.send({
+    msg:'POST request'
+  });
+});
+
+// PUTリクエスト
+app.put('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'PUT request'
+  });
+});
+
+// DELETEリクエスト
+app.delete('/:id', (req, res) => {
+  res.send({
+    id: req.params.id,
+    msg:'DELETE request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
